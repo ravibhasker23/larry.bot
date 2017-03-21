b252f432adf9dbbf87498d3ecfee7f2175559920
ravibhasker23 <ravibhasker23@gmail.com>
2017-03-21 17:58:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def makeWebhookResult(req):
            
     #speech =  "Technology " + technology + " Project Location " + projectLoc + "Role " + designation
     #speech  = "You have searched profiles for " + technology + " for location " + prefLoc + " with experience " + experience + " and designation " + designation
-    speech = tech + " reosources " + location + " " + role 
+    speech = "hello world " + technology 
     
     print("Response:")
     print(speech)
