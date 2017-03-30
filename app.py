79cdc06679e3baa28043da800bba650a799d17c1
ravibhasker23 <ravibhasker23@gmail.com>
2017-03-30 17:23:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import json
 import os
 import MySQLdb
 
+from flask import Flask
+from flask import request
+from flask import make_response
+
 db = MySQLdb.connect(host = "122.98.107.190",user = "POC_USER",passwd = "pocuser",db = "Informatica")
 
 cur = db.cursor()
@@ -16,9 +20,7 @@ for row in cur.fetchall():
 
 db.close()
 
-from flask import Flask
-from flask import request
-from flask import make_response
+
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -26,43 +28,36 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+        req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+        print("Request:")
+        print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+        res = makeWebhookResult(req)
 
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+        res = json.dumps(res, indent=4)
+        print(res)
+        r = make_response(res)
+        r.headers['Content-Type'] = 'application/json'
+        return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "chatbot.test":
-        return {}
-    
-    result = req.get("result")
-    
-    parameters = result.get("parameters")
-    
-    
-
-    designation = parameters.get("role")
-    if designation is None:
-        return {}
+        if req.get("result").get("action") != "chatbot.test":
+                return {}
+        result = req.get("result")
+        parameters = result.get("parameters")
+        designation = parameters.get("role")
+        if designation is None:
+                return {}
     #speech = "Technology you want" + technology + " Project Location " + projectLoc + "Role " + designation
-    
-    projectLoc = parameters.get("location")
-    if projectLoc is None:
-        return {}
+        projectLoc = parameters.get("location")
+        if projectLoc is None:
+                return {}
     #speech = "Technology you want" + technology + " Project Location " + projectLoc
 
-    technology = parameters.get("technology") 
-    if technology is None:
-        return {}
-    
+        technology = parameters.get("technology") 
+        if technology is None:
+                return {}
     #speech = "Technology you want" + technology
 
     #resource = {'JAVA' : 10, '.Net' : 2, 'Blue Prism' : 5}
@@ -71,23 +66,22 @@ def makeWebhookResult(req):
     #speech = speech1 + " hence connected"
     #speech  = "You have searched profiles for " + technology + " for location " + prefLoc + " with experience " + experience + " and designation " + designation
     
-    speech = "Technology you want" + technology + " Project Location " + projectLoc + "Role " + designation
-    
-    print("Response:")
-    print(speech)
+        speech = "Technology you want" + technology + " Project Location " + projectLoc + "Role " + designation
+        print("Response:")
+        print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "larry.bot"
-    }
+        return {
+                "speech": speech,
+                "displayText": speech,
+                #"data": {},
+                # "contextOut": [],
+                "source": "larry.bot"
+        }
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+        if __name__ == '__main__':
+        port = int(os.getenv('PORT', 5000))
 
     #print "Starting app on port %d" % port
  
-    app.run(debug=True, port=port, host='0.0.0.0')
+        app.run(debug=True, port=port, host='0.0.0.0')
